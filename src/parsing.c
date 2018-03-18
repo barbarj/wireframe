5c8f84635c62f71039ffffd6b88ca42727f8d80d
RJ Hallsted <rhallste@student.42.us.org>
2018-03-17 21:45:54 -0700

    more norm

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/16 00:08:39 by rhallste          #+#    #+#             */
-/*   Updated: 2018/03/17 20:48:45 by rhallste         ###   ########.fr       */
+/*   Updated: 2018/03/17 21:45:23 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,35 @@ static size_t			array_len(char **words)
 	count = 0;
 	while (words[count])
 		count++;
-	return count;
+	return (count);
 }
 
-static t_fdf_point3d	*get_line_points(int fd, unsigned int *width, int line_index)
+static t_fdf_point3d	set_point(unsigned int i, int line_index,
+								char *alt_str)
+{
+	char			**param_split;
+	t_fdf_point3d	point;
+
+	point.x = i * FDF_POINT_SEP;
+	point.y = line_index * FDF_POINT_SEP;
+	param_split = ft_strsplit(alt_str, ',');
+	point.z = ft_atoi(param_split[0]) * FDF_POINT_SEP;
+	ft_free_2d_array((void ***)&param_split, (param_split[1]) ? 2 : 1);
+	return (point);
+}
+
+static t_fdf_point3d	*get_line_points(int fd, unsigned int *width,
+										int line_index)
 {
 	char			*line;
 	t_fdf_point3d	*points;
 	char			**alt_strs;
-	char			**param_split;
 	unsigned int	i;
 
 	if ((get_next_line(fd, &line)) < 1)
 		return (NULL);
 	alt_strs = ft_strsplit(line, ' ');
+	free(line);
 	if (line_index == 0)
 		*width = array_len(alt_strs);
 	if (!(points = ft_memalloc(sizeof(t_fdf_point3d) * *width)))
@@ -44,15 +59,10 @@ static t_fdf_point3d	*get_line_points(int fd, unsigned int *width, int line_inde
 	i = 0;
 	while (i < *width && alt_strs[i])
 	{
-		points[i].x = i * FDF_POINT_SEP;
-		points[i].y = line_index * FDF_POINT_SEP;
-		param_split = ft_strsplit(alt_strs[i], ',');
-		points[i].z = ft_atoi(param_split[0]) * FDF_POINT_SEP;
-		ft_free_2d_array((void ***)&param_split, (param_split[1]) ? 2 : 1);
+		points[i] = set_point(i, line_index, alt_strs[i]);
 		i++;
 	}
 	ft_free_2d_array((void ***)&alt_strs, i);
-	free(line);
 	if (i < *width)
 		fdf_shortline_error();
 	return (points);
@@ -61,21 +71,21 @@ static t_fdf_point3d	*get_line_points(int fd, unsigned int *width, int line_inde
 t_fdf_point3d			**fdf_parse_file(int fd, unsigned int *width,
 									unsigned int *height)
 {
-	t_fdf_point3d 	*line_points;
+	t_fdf_point3d	*line_points;
 	t_fdf_point3d	**points;
-	size_t			arr_size_new;
-	size_t			arr_size_old;
-	
+	size_t			arsz_new;
+	size_t			arsz_old;
+
 	if (!(points = ft_memalloc(sizeof(t_fdf_point3d *))))
 		return (NULL);
 	*height = 0;
-	arr_size_old = 0;
+	arsz_old = 0;
 	while ((line_points = get_line_points(fd, width, *height)))
 	{
-		arr_size_new = arr_size_old + sizeof(t_fdf_point3d *);
-		if (!(points = ft_memrealloc((void *)points, arr_size_new, arr_size_old)))
+		arsz_new = arsz_old + sizeof(t_fdf_point3d *);
+		if (!(points = ft_memrealloc((void *)points, arsz_new, arsz_old)))
 			return (NULL);
-		arr_size_old = arr_size_new;
+		arsz_old = arsz_new;
 		points[*height] = line_points;
 		(*height)++;
 	}
